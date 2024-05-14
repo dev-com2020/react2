c4a9f9a13174e2e6c665b8a5f51641c61ac87c6a
tomaszkaniecki <kwp@tlen.pl>
2024-05-14 15:04:28 +0200

    routery

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MyFeature from './MyFeature'
+import {RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import MyComponent from './MyComponent'
+import MyFeature from './MyFeature'
+
+const router = createBrowserRouter([
+  {path:"/", element: <MyComponent/>},
+  {path:"/blog", element: <MyFeature />}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MyFeature />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
