23eae5547958ad80c38799ab918d30c40db4b670
tomaszkaniecki <kwp@tlen.pl>
2024-05-15 07:56:15 +0200

    routing

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -5,10 +5,14 @@ import './index.css'
 import MyComponent from './MyComponent'
 import MyFeature from './MyFeature'
 
+// https://drive.proton.me/urls/99JEVR17YM#8tlX945Qgyar
+
 const router = createBrowserRouter([
   {path:"/", element: <MyComponent/>},
   {path:"/blog", element: <MyFeature />}
 ])
+
+// npm install react-router-dom
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
